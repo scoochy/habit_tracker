ffdb76449f5a72da2913110ad0067696d031b664
Scoochy <52045665+scoochy@users.noreply.github.com>
2025-05-19 15:27:35 +0700

    relative imports

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from datetime import date
-import models, schemas, crud
+from . import models, schemas, crud
 from database import engine, SessionLocal
 
 
